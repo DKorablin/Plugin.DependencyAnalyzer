6b23f31f1187a835f9cc92dfc97907fe4a4924ab
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:47:52 +0300

    Unified plugins deployment

--- Plugin.DependencyAnalyzer/Properties/AssemblyInfo.cs
+++ Plugin.DependencyAnalyzer/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("cfb29f05-83eb-41db-b0b7-ae54f8b15863")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,16 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=121")]
 #else
 
-[assembly: AssemblyTitle("Plugin.DependencyAnalyzer")]
 [assembly: AssemblyDescription(".NET Assembly reference analyzer with Microsoft Automatic Graph Layout and PEReader")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.DependencyAnalyzer")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2025")]
 #endif
 
 /*if $(ConfigurationName) == Release (
